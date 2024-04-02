e9c1de889716a232b8e50c5cd8785c7a5dcd292a
dlrtmfpdls0!@ <asdfg002269@gmail.com>
2024-04-03 00:06:02 +0900

    aa

--- src/main/resources/static/javascript/side.js
+++ src/main/resources/static/javascript/side.js
@@ -1,10 +1,16 @@
 
 $(function(){
     $(".inventory").on("click",function(){
-
         $(this).next($(".inventory_sub_menu")).slideToggle();
-
     });
-
+    $(".designer").on("click",function(){
+        $(this).next($(".designer_sub_menu")).slideToggle();
+    });
+    $(".customer").on("click",function(){
+        $(this).next($(".customer_sub_menu")).slideToggle();
+    });
+    $(".shop").on("click",function(){
+        $(this).next($(".shop_sub_menu")).slideToggle();
+    });
 
 });
