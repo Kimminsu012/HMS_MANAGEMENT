42168e86dccfcef862e91397ebd3663f7935cb3e
minjae021125 <iminjae399@gmail.com>
2024-04-08 17:31:56 +0900

    HMS 프로젝트

---
+++ src/main/resources/static/javascript/invenList.js
@@ -0,0 +1,60 @@
+
+$(function() {
+    $("#modal_wrap").hide();
+    $(".buyBt, .sortBt, .editBt").on("click", function() {
+        $("#modal_wrap").show();
+        var buttonText = $(this).text();
+        $(".modal_text").text(buttonText);
+    });
+
+
+    $(".modal_bt").on("click", function() {
+        $("#modal_wrap").hide();
+    });
+
+    $(".deleteBt").on("click",function(){
+        $(".list_chk").show();
+        $(".buyBt, .sortBt, .editBt").hide();
+
+        $(".all_chk").on("click",function(){
+           $(".list_chk").prop("checked" , $(this).prop("checked"));
+
+        });
+    });
+
+    var allCheck = $(".all_chk");
+    var listCheck = $(".list_content_text .list_chk");
+
+    allCheck.on("click", function() {
+        if($(this).is(":checked")) {
+            listCheck.prop("checked", true);
+
+        }else{
+            listCheck.prop("checked", false);
+        }
+    });
+    listCheck.on("click", function() {
+        var allCheckedBox = true;
+        listCheck.each(function(){
+            if (!$(this).is(":checked")) {
+                allCheckedBox = false;
+                return false;
+            }
+        });
+        allCheck.prop("checked", allCheckedBox);
+    });
+
+    $(".deleteGo").on("click",function(){
+       if($(".list_chk:checked").length > 0){
+           if(confirm("정말 삭제 하시겠습니까?")){
+               $(this).parents('.list_content_text').remove();
+               alert("삭제 되었습니다.");
+           }
+       }else{
+           alert("삭제할 데이터를 체크 해주세요.");
+       }
+
+    });
+
+
+});
--- src/main/resources/static/javascript/side.js
+++ src/main/resources/static/javascript/side.js
@@ -1,41 +1,73 @@
 
 $(function(){
+
+    var url = location.href;
+    var ex = true;
+    if( url.indexOf("/inven") > 0 ) {
+        $(".inventory_sub_menu").show();
+        $(".inven_border").show();
+    }
+    else if( url.indexOf("/designer") > 0){
+        $(".designer_sub_menu").show();
+        $(".designer_border").show();
+    }
+    else if( url.indexOf("/customer") > 0 ){
+        $(".customer_sub_menu").show();
+        $(".customer_border").show();
+    }
+    else if( url.indexOf("/shop") > 0){
+        $(".shop_sub_menu").show();
+        $(".shop_border").show();
+    }else if( url.indexOf("/sales") > 0){
+        $(".sales_border").show();
+    }
+
     $(".inventory").on("click",function(){
+        ex = !ex;
         $(this).next($(".inventory_sub_menu")).slideToggle();
     });
     $(".designer").on("click",function(){
+        ex = !ex;
         $(this).next($(".designer_sub_menu")).slideToggle();
     });
     $(".customer").on("click",function(){
+        ex = !ex;
         $(this).next($(".customer_sub_menu")).slideToggle();
     });
     $(".shop").on("click",function(){
+        ex = !ex;
         $(this).next($(".shop_sub_menu")).slideToggle();
     });
 
+    // $(document).on('click', 'a', function(e) {
+    //     if (!ex) {
+    //         e.preventDefault();
+    //     }
+    // });
 
-    $(document).ready(function() {
 
-        $(document).on('click', 'a', function(e) {
+    // $(document).ready(function() {
+    //
+    //     $(document).on('click', 'a', function(e) {
+    //
+    //         if ($(this).attr('href').startsWith('#') || $(this).attr('href').startsWith('http')) {
+    //             return;
+    //         }
+    //
+    //         e.preventDefault();
+    //
+    //         var url = $(this).attr('href');
+    //
+    //         $.ajax({
+    //             url: url,
+    //             success: function(data) {
+    //                 $('#content').html($(data).find('#content').html());
+    //                 $('.sub_menu a[href="' + url + '"]').parent().show().parent().show();
+    //             }
+    //         });
+    //     });
+    //
+    // });
 
-            if ($(this).attr('href').startsWith('#') || $(this).attr('href').startsWith('http')) {
-                return;
-            }
-
-            e.preventDefault();
-
-            var url = $(this).attr('href');
-
-            $.ajax({
-                url: url,
-                success: function(data) {
-                    $('#content').html($(data).find('#content').html());
-                    $(".sub_menu a").style("")
-                    $('.sub_menu a[href="' + url + '"]').parent().show().parent().show();
-                }
-            });
-        });
-
-    });
 
 });
